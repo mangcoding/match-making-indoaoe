7f3bc5e32d7a7d6b2d3086e7c3b1fe37fa0b5b3c
SamsulAripin77 <arfins192@gmail.com>
2024-11-01 10:38:11 +0700

    before swithch to crown

--- config/app.php
+++ config/app.php
@@ -41,8 +41,9 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    // 'debug' => (bool) env('APP_DEBUG', true),
 
+    'debug' => true,
     /*
     |--------------------------------------------------------------------------
     | Application URL
