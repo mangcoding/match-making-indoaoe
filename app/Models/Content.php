def352e33a89a8b9ba2e9efd6b5a0005b11d7e74
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-11-19 22:28:22 +0700

    feature(model): add content model

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Content extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "groups_id",
+        'field_types',
+        'field_value',
+        'label',
+        'link'
+    ];
+
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class, 'groups_id', 'id');
+    }
 }
