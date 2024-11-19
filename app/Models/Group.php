6ced9c53b19c3e33dfc18e55d32a64144ebabc06
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-11-19 22:27:44 +0700

    feat(model): add relation to group model

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Group extends Model
@@ -16,8 +17,8 @@ class Group extends Model
         'name'
     ];
 
-    public function contents()
+    public function contents(): HasMany
     {
-        return $this->hasMany(Content::class);
+        return $this->hasMany(Content::class, 'groups_id', 'id');
     }
 }
