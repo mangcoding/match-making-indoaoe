725941ff708db62131f2011eecf119f2aa1b6fb2
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-11-19 22:28:42 +0700

    feat(migration): add contents migration

---
+++ database/migrations/2024_11_19_152215_create_contents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('groups_id')->constrained('groups')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('field_types', 150);
+            $table->text('field_value');
+            $table->string('label', 150);
+            $table->string('link', 100)->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
