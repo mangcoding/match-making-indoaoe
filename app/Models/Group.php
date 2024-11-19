439752e7bc742de543e4f23ff8acbfa18bd76b04
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-11-19 22:16:54 +0700

    feat(model): add Group model

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,14 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Group extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
+    protected $fillable = [
+        'page',
+        'slug',
+        'name'
+    ];
 
-    public function contents () {
+    public function contents()
+    {
         return $this->hasMany(Content::class);
-
     }
 }
