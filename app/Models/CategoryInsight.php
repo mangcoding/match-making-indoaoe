1b9c9b4ea74e2f981b100b4a677d8de3bedf5e0a
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-11-20 10:12:06 +0700

    fix(model): fix relation

--- app/Models/CategoryInsight.php
+++ app/Models/CategoryInsight.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CategoryInsight extends Model
@@ -11,4 +13,9 @@ class CategoryInsight extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['name'];
+
+    public function insights(): BelongsToMany
+    {
+        return $this->belongsToMany(Insight::class, 'insight_categories', 'category_id', 'insight_id');
+    }
 }
