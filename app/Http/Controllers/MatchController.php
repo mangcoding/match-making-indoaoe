8fc2be9157b6eaccea743acc10754b90a57d7db6
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-11-26 07:16:24 +0700

    refactor(controller): remove date filter on game_matches query

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -84,45 +84,45 @@ class MatchController extends Controller
 
     public function index()
     {
-
-
-        // Cache players in 1 hour
         $sql = "select
-	tbl.*,
-	((tbl.win - tbl.lose) * 0.5) + tbl.elo as final_elo
-from
-	(
-	select
-		p.aoe2net_id,
-		p.name,
-		p.alias,
-		p.elo,
-		p.avatar_url,
-		p.streak,
-		p.crown,
-		count(*) as total_matches,
-		count(*) filter (
-		where gm.won = true) as win,
-		count(*) filter (
-		where gm.won = false) as lose
-	from
-		players p
-	left join (select
-	*
-from
-	game_matches g
-inner join match_teams t on
-	t.match_id = g.match_id
-where
-	g.started_at >= '2024-06-01 00:00:00'
-	and g.started_at is not null) as gm on gm.aoe2net_id = p.aoe2net_id
-	where
-		p.aoe2net_id is not null
-		and p.status = '1'
-	group by
-		p.id) as tbl order by final_elo desc;";
-        $players = DB::select($sql);
+    tbl.*,
+    ((tbl.win - tbl.lose) * 0.5) + tbl.elo as final_elo
+    from
+    (
+    select
+        p.aoe2net_id,
+        p.name,
+        p.alias,
+        p.elo,
+        p.avatar_url,
+        p.streak,
+        p.crown,
+        count(*) as total_matches,
+        count(*) filter (
+        where gm.won = true) as win,
+        count(*) filter (
+        where gm.won = false) as lose
+    from
+        players p
+    left join (
+        select
+            *
+        from
+            game_matches g
+        inner join match_teams t on
+            t.match_id = g.match_id
+        where
+            g.started_at is not null
+    ) as gm on gm.aoe2net_id = p.aoe2net_id
+    where
+        p.aoe2net_id is not null
+        and p.status = '1'
+    group by
+        p.id
+    ) as tbl
+    order by final_elo desc;";
 
+        $players = DB::select($sql);
 
         // add rank key using map
         $players = array_map(function ($player, $index) {
